75f4aab5a389b915da76acb1d2db6b9897ef8c04
valentina-baseotto <91380643+valentina-baseotto@users.noreply.github.com>
2024-02-18 16:45:59 +0100

    tabla de planificacion

--- Practica1/planificacion.php
+++ Practica1/planificacion.php
@@ -13,9 +13,42 @@
             <?php include("componentes/header.php"); ?>
         </div>
         <h1>Planificacion</h1>
+        <p>En esta página se describe la planificación del proyecto.</p>
         <section>
             <h2>Planificacion de la practica</h2>
-            
+            <table>
+               <tbody>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Fecha</th>
+                        <th>Actividad</th>
+                    </tr>
+                    <tr>
+                        <td>Practica 1</td>
+                        <td>23/02/2024</td>
+                        <td>Pagina web sencilla, formada por varios documentos, incluyendo imágenes, tablas y formularios</td>
+                    </tr>
+                    <tr>
+                        <td>Practica 2</td>
+                        <td>8/03/2024</td>
+                        <td>Arquitectura y prototipo funcional del proyecto.</td>
+                    </tr>
+                    <tr>
+                        <td>Practica 3</td>
+                        <td>12/04/2024</td>
+                        <td>Diseño del proyecto y más funcionalidades</td>
+                    </tr>
+               </tbody> 
+            </table>
+        </section>
+        <section>
+            <h2>Diagrama de Gantt</h2>
+            <h3>Practica 1</h3>
+            <img src="img/gantt1.png" alt="Gantt1" width="500" height="300">
+            <h3>Practica 2</h3>
+            <img src="img/gantt2.png" alt="Gantt2" width="500" height="300">
+            <h3>Practica 3</h3>
+            <img src="img/gantt3.png" alt="Gantt3" width="500" height="300">
         </section>
         <script src="js/componentes.js"></script>
     </body>
