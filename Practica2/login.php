4d9b47566712532813be1a3e2ae999cfcbee04cb
marcosgcr3 <marcosgcr3@gmail.com>
2024-02-21 11:34:35 +0100

    arreglo

--- Practica2/login.php
+++ Practica2/login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 $modoOscuro = isset($_COOKIE['modoOscuro']) && $_COOKIE['modoOscuro'] === 'activado';
@@ -57,64 +56,4 @@ $modoOscuro = isset($_COOKIE['modoOscuro']) && $_COOKIE['modoOscuro'] === 'activ
         
         
     </body>
-=======
-<?php
-session_start();
-$modoOscuro = isset($_COOKIE['modoOscuro']) && $_COOKIE['modoOscuro'] === 'activado';
-?>
-
-<!DOCTYPE html>
-<html lang="es">
-    <head>
-        <?php
-        
-            if ($modoOscuro) {
-                echo '<link id ="estilo" rel="stylesheet" href="css/indexNight.css">';
-            }else{
-                echo '<link id ="estilo" rel="stylesheet" href="css/index.css">';}
-       ?>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="Index" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"/>
-        <title>DriveCrafters</title>
-    </head>
-   
-    <body> 
-
-        <div class="container-encabezado">
-          <?php include("componentes/header.php"); ?>
-
-        </div> 
-
-        <div class="container-login">
-            <h2>Login Usuario</h2>
-
-            <form action="ProcesaLogin.php" method="post">
-                
-                <p>NIF:</p>
-                <input type="text" name="NIF" id="NIF" required>
-                <p>Contraseña:</p>
-                
-                <input type="password" name="password" id="password" required>
-                <p></p>
-                <input type="submit" value="Registrarse">
-            </form>
-        </div>
-        
-        
-
-       
-
-        
-        <footer>
-            <p>&copy; 2024 DriveCrafters - Todos los derechos reservados</p>
-        </footer>
-        
-        <script src="js/cabecera.js"></script>
-        <script src="js/index.js"></script>
-        
-        
-    </body>
->>>>>>> c3d2392ea84a67af5b94d706eb9fe1d48bf813d7
 </html>
--- Practica2/logout.php
+++ Practica2/logout.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 unset($_SESSION['login']);
@@ -8,15 +7,4 @@ unset($_SESSION['esMecanico']);
 session_destroy();
 header("Location: index.php");
 
-=======
-<?php
-session_start();
-unset($_SESSION['login']);
-unset($_SESSION['nombre']);
-unset($_SESSION['esAdmin']);
-unset($_SESSION['esMecanico']);
-session_destroy();
-header("Location: index.php");
-
->>>>>>> c3d2392ea84a67af5b94d706eb9fe1d48bf813d7
 ?>
