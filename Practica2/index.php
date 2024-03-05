6624c7650b4ed9fc66e2e9e272a77eb9298f0b8d
MeraKy43 <papadial@ucm.es>
2024-03-05 10:41:23 +0100

    He modificado el contacto.php que no era la version final y he cambiado el index.php

--- Practica2/index.php
+++ Practica2/index.php
@@ -9,7 +9,15 @@ $contenidoPrincipal=<<<EOS
 <div class="imagen">
 <img src="img/foto1.jpg" id="imagenPrincipal" alt="centrado">
 </div>
-<p> Aquí está el contenido público, visible para todos los usuarios. </p>
+<p> 
+    Los mejores artículos para tu coche al mejor precio del mercado.
+    Podrás ser recomendado por nuestros increibles mecánicos expertos en mantenimieto
+    y acondicionamiento de automóviles. Además, estos podrán repara tu vehículo, 
+    realizarle algunas mejoras técnicas o hacerle la revisión de la ITV. También puedes disponer 
+    de un servicio de alquiler de automóviles en caso de no disponer de uno o simplemente 
+    poder conducir el coche que siempre deseaste. No lo dudes y confía en DRIVECRAFTERS.
+</p>
+<button class="botonIni" onclick="location.href='entrar.php'">LOGIN/REGISTER</button>
 EOS;
 
 require 'includes/componentes/layout.php';
