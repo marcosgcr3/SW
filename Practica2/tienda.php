1beb67e4ee4deec8916adb4411533655c7a81f0f
Ppadial <84508086+MeraKy43@users.noreply.github.com>
2024-03-10 18:52:22 +0100

    tienda.php

--- Practica2/tienda.php
+++ Practica2/tienda.php
@@ -4,40 +4,24 @@ require_once 'includes/config.php';
 require_once 'includes/design/plantillas/producto.php';
 require_once 'includes/acceso/autorizacion.php';
 require_once 'includes/src/Producto/producto.php';
+require_once 'includes/src/listado_tienda.php';
 $tituloPagina = 'Tienda';
 
-
-
-
+$contenidoPrincipal = '';
 $conn = BD::getInstance()->getConexionBd();
 $sql = "SELECT * FROM productos";
 $result = $conn->query($sql);
 
-
-$contenidoPrincipal = '';
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        $contenidoPrincipal .= buildArticulo($row['nombre'], $row['precio'], $row['descripcion'], $row['unidades'], $row['imagen']);
-        
-    
+        $contenidoPrincipal .= elArticulo($row);
     }
     $result->free();
 } else {
-    $contenidoPrincipal .= "<tr><td colspan='4'>No hay productos disponibles</td></tr>";
+    $contenidoPrincipal .= sinArticulos();
 }
 
 if( esAdmin() ){
-    $contenidoPrincipal.=<<<EOS
-    
-    <button class="botonIni" onclick="location.href='addProducto.php'">Añadir producto</button>
-    EOS;
-   
-    
-
-
+    $contenidoPrincipal.= añadirProducto();
 }
 require 'includes/design/comunes/layout.php';
-
-
-
-
