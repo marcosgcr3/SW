b42295d7c6c9c1ea87a49e8d51dc4a4268287df4
valentina <91380643+valentina-baseotto@users.noreply.github.com>
2024-03-19 21:34:14 +0100

    Update producto.php

--- Practica3/includes/vistas/plantillas/producto.php
+++ Practica3/includes/vistas/plantillas/producto.php
@@ -2,18 +2,21 @@
 
 require_once 'includes/config.php';
 use es\ucm\fdi\aw\Aplicacion;
+use es\ucm\fdi\aw\productos\Producto;
+use es\ucm\fdi\aw\pedidos\Pedidos;
+
 
 function buildArticulo($producto)
 {
     $productos = '';
         
-    $cantidad = 1;
     $nombre = $producto->getNombre();
     $unidades = $producto->getUnidades();
     $precio = $producto->getPrecio();
-    $idCantidad = "cantidad_$nombre"; // Identificador único para el campo de cantidad
+    $id_producto = $producto->getId();
     $imagen = $producto->getImagen();
     $descripcion = $producto->getDescripcion();
+    $id_usuario = $_SESSION['id'];
     $productos .=  <<<EOS
     <div class="producto">
         <div class="producto-info">
@@ -23,40 +26,18 @@ function buildArticulo($producto)
                 <p>$descripcion</p>
                 <p>Precio: $precio&euro;</p>
                 <p>Unidades disponibles: $unidades</p>
-                <!-- Botones para ajustar cantidad -->
-                <div class="cantidad-botones">
-                    <button class="boton-disminuir" onclick="disminuirCantidad('$idCantidad', $unidades)">-</button>
-                    <span id="$nombre"> $cantidad </span>
-                    <button class="boton-aumentar" onclick="aumentarCantidad('$idCantidad', $unidades)">+</button>
-                    <p>$cantidad</p>
-                </div> 
+                <!-- FORM -->
+                <form action="addProductoAlCarro.php" method="POST">
+                    <input type="hidden" name="id" value={$_SESSION['id']}>
+                    <input type="hidden" name="id_producto" value="$id_producto">
+                    <input type="number" name="unidades" min="1" max="$unidades">
+                    <button type="submit">Añadir al carrito</button>
+                </form>
+            </div>
+            
+    </div></div></div>';
     EOS;
-       
-    $app = Aplicacion::getInstance();
-    if ($app->esAdmin()) {
-        // Enlace para sumar stock con método POST
-        $productos .= <<<EOS
-            <form id="formSumarStock_$nombre" action="sumarStock.php" method="post">
-                <input type="hidden" name="nombre" value="$nombre">
-                <input type="hidden" name="cantidad" id="$idCantidad" value="$cantidad"> <!-- Agrega un ID al campo de cantidad -->
-                sumarStock('$nombre')
-                <button type="submit" class="botoncarro">Sumar</button>
-            </form>
-            <form id="formBorrarProducto_$nombre" action="borrarProducto.php" method="post">
-                <input type="hidden" name="nombre" value="$nombre">
-            </form>
-            <i id="iconoBasura" class="fa-solid fa-trash" onclick="document.getElementById('formBorrarProducto_$nombre').submit();"></i>
-        </div></div></div>
-        EOS;
-    } else {
-        // Botón para añadir al carrito
-        $productos .= <<<EOS
-            <button class="botoncarro" onclick="location.href='addProductoAlCarro.php?nombre=$nombre&unidades=$cantidad';">Añadir al carrito</button>
-        </div></div></div>
-        EOS;
-    }
+        
                 
     return $productos;
 }
-
-
