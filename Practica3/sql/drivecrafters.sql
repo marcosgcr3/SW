fca5f1b6a90383076c47f58a15b8c201762c1726
Ppadial <84508086+MeraKy43@users.noreply.github.com>
2024-04-08 10:48:11 +0200

    Columna estado citas

--- Practica3/sql/drivecrafters.sql
+++ Practica3/sql/drivecrafters.sql
@@ -52,7 +52,8 @@ CREATE TABLE `citas` (
   `id_mecanico` int(11) NOT NULL,
   `dia` date NOT NULL,
   `hora` int(11) NOT NULL,
-  `asunto` text NOT NULL
+  `asunto` text NOT NULL,
+  `estado` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
