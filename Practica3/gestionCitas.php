cd5cb4a9f04a67fa09fd24a3bc7321238a92acda
marcosgcr3 <marcosgcr3@gmail.com>
2024-04-12 11:01:28 +0200

    Update gestionCitas.php

--- Practica3/gestionCitas.php
+++ Practica3/gestionCitas.php
@@ -6,28 +6,21 @@ require_once 'includes/config.php';
 require_once 'includes/src/citas/listado_citas.php';
 $tituloPagina = 'Gestionar citas';
 
-// Función para listar las citas del mecánico seleccionado
+
+$idMecanico = filter_input(INPUT_POST, 'mecanico', FILTER_SANITIZE_SPECIAL_CHARS);
 
 
-// Si se ha enviado el formulario con el ID del mecánico seleccionado
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["accion"]) && $_POST["accion"] == "seleccionar_mecanico") {
-    // Obtener el ID del mecánico seleccionado
-    $idMecanico = $_POST["mecanico"];
-    // Llamar a la función listaCitasMecanico con el ID del mecánico
-    
-}
-
-// Obtener la lista de mecánicos
 $mecanicos = Usuario::listaMecanicos();
 
-// Construir el desplegable con los nombres de los mecánicos
+
 $selectMecanicos = '<select name="mecanico">';
+$selectMecanicos .= '<option value="-">-</option>'; 
 foreach($mecanicos as $mecanico){
     $selectMecanicos .= '<option value="' . $mecanico->getId() . '">' . $mecanico->getNombre() . '</option>';
 }
 $selectMecanicos .= '</select>';
 
-// Formulario oculto para enviar los datos mediante POST
+
 $formulario = <<<HTML
 <form id="formSeleccionMecanico" action="{$_SERVER['PHP_SELF']}" method="post">
     <input type="hidden" name="accion" value="seleccionar_mecanico">
