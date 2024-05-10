bd48760f8cb20ac442dd956d03d04212570fd69f
marcosgcr3 <marcosgcr3@gmail.com>
2024-05-10 12:19:54 +0200

    admin puede ver citas de mecanicos

--- Practica4/calendario.php
+++ Practica4/calendario.php
@@ -2,17 +2,50 @@
 require_once __DIR__.'/includes/config.php';
 $tituloPagina = "Calendario";
 $ev = filter_input(INPUT_GET, 'tipo', FILTER_SANITIZE_SPECIAL_CHARS);
+use es\ucm\fdi\aw\usuarios\Usuario;
+
+
+if($app->esAdmin()){
+  
+  $mecanicos = Usuario::listaMecanicos();
+
+  $contenidoPrincipal = '';
+
+  $idMecanico = ''; // Inicializar la variable
+
+  if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['mecanico'])) {
+      $idMecanico = $_GET['mecanico']; // Guardar el ID del mecánico seleccionado
+  }
+
+  $contenidoPrincipal = '';
+
+  $contenidoPrincipal .= <<<HTML
+  <form id="formSeleccionMecanico" action="{$_SERVER['PHP_SELF']}" method="get">
+      <input type="hidden" name="accion" value="seleccionar_mecanico">
+      <select class="selectMecanico" name="mecanico">
+          <option value="-">-</option>';
+  HTML;
+
+  foreach($mecanicos as $mecanico){
+      $selected = ($idMecanico == $mecanico->getId()) ? 'selected' : ''; // Marcar como seleccionado si es el mecánico actual
+      $contenidoPrincipal .= '<option value="' . $mecanico->getId() . '" ' . $selected . '>' . $mecanico->getNombre() . '</option>';
+  }
+
+  $contenidoPrincipal .= <<<HTML
+      </select>
+      <button class ="botonSeleccionarMecanico" type="submit">Seleccionar</button>
+  </form>
+  HTML;
+  $ev = 'eventosAdmin.php?idMecanico=' . $idMecanico;
+}
 
 /*if($app->esCliente()){
   $ev = 'misCitas.php';
 }*/
-if($app->esMecanico()){
-  $rol = 'mecanico';
-}
 
 
 
-$contenidoPrincipal = <<<HTML
+$contenidoPrincipal .= <<<HTML
 
 <head>
   <title>JQuery Full Calendar</title>
