2b7fc98c2d9751684ce3f2f9fd971d2826e779e4
valentina-baseotto <91380643+valentina-baseotto@users.noreply.github.com>
2024-05-12 16:58:32 +0200

    ya esta

--- Practica4/deleteProductoCarro.php
+++ Practica4/deleteProductoCarro.php
@@ -27,7 +27,7 @@ else{//ya tiene carrito este usuario, lo compramos
     else{
         $pedido->eliminarProductos($id_pedido,$id_producto, $unidades);
     }
-    $producto->devolverProductos($id_producto, $unidades);  
+    //$producto->devolverProductos($id_producto, $unidades);  
     if(empty($pedido)){
         $pedido->borrarPedido_producto($pedido->getId_pedido());
         $pedido->borrarPedido($pedido->getId_pedido()); 
